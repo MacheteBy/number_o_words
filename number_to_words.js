be51faa0fe154ee78647946055157bf5516b7646
MacheteBy <Macheteby@gmail.com>
2023-03-10 23:56:09 +0300

    last fix

--- number_to_words.js
+++ number_to_words.js
@@ -2,12 +2,12 @@ var rubles = require('rubles').rubles;
 
 // первый вариан
 
-var text = rubles(898); // <--- cюда вводи переменную
-console.log(text.replace(' рублей 00 копеек', ''));
-
+var text = rubles(781); // <--- cюда вводи переменную
+// console.log(text.replace(' рублей 00 копеек', ''));
+console.log(text.replace(/\sр\у([\d\w\W]*)/, '').trim());
 
 // второй вариант через переменную
 
 let num = 88; // <--- cюда вводи переменную
 var text =  rubles(num);
-console.log(text.replace(' рублей 00 копеек', ''));
+console.log(text.replace(/\sр\у([\d\w\W]*)/, '').trim());
